5e2fc620c0b0a18bfe47b875ab43ec07a6894b2b
imSmaity <sumanmaity305@gmail.com>
2024-02-17 11:01:50 +0530

    fix: radial gradient

--- app/page.js
+++ app/page.js
@@ -1,14 +1,20 @@
 import './style.css'
+import FastMarquee from 'react-fast-marquee'
 
 export default function Home() {
   return (
     <main>
       <div className="gradient-background">
         <div className="flex justify-center items-center h-[45rem]">
-          <marquee className="text-[220px] font-semibold" scrollamount="15">
+          <FastMarquee
+            style={{ fontSize: 220 }}
+            speed={200}
+            delay={1}
+            className={'font-semibold'}
+          >
             ZEIGEN WIE’s GEHT
-          </marquee>
-          <div className="w-[205px] h-[205px] p-10 top-[23rem] right-[18rem] rounded-full flex flex-col justify-center items-center gap-5 absolute bg-black text-white">
+          </FastMarquee>
+          <div className="w-[205px] h-[205px] p-10 top-[23rem] right-[18rem] rounded-full flex flex-col justify-center items-center gap-5 absolute bg-black text-white z-50">
             <div className="uppercase text-[22px] text-center">
               Creative Days 2024
             </div>
